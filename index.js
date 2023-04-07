7a88ee41e7f7bb393a0922497515a34137cefc8e
Dennis-Inglehart <Haikon@twosixfour.org>
2023-04-07 15:21:26 -0400

    add list question and add placeholder module.export

--- index.js
+++ index.js
@@ -1,52 +1,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { exec } = require('node:child_process');
+const generateMarkdown = require('./utils/generateMarkdown');
+const introString = "Inglehart readme generator ver.0.1.0\n------------------------------------\n"
 
-// TODO: Create an array of questions for user input
-const questions = [
-    "What is the TITLE of your project? ",
-    "What is the DESCRIPTION of your project? ",
-    "What are the USAGE instructions for your project? ",
-    "How can users make a CONTRIBUTION? ",
-    "What are the TEST INSTRUCTIONS? "
-];
+// TODO: Create an array of questions for user input <- this comment came with the starter code
+const contentQuestions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the TITLE of your project? "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the DESCRIPTION of your project? "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the USAGE instructions for your project? "
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can users make a CONTRIBUTION? "
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the TEST INSTRUCTIONS? "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Under what LICENSE is your project? ",
+        choices: [
+            'MY PROJECT HAS NO LICENSE!',
+            'Apache Liscence 2.0',
+            'GNU General Public License v3.0',
+            'MIT LicenseBSD 2-Clause "Simplified" License',
+            'MIT LicenseBSD 3-Clause "New" or "Revised" License',
+            'Boost Software License 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense',
+        ]
+    }]
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file <- this comment came with the starter code
 function writeToFile(fileName, data) {
     let writeYou = fs.createWriteStream(fileName);
     writeYou.write(data);
 }
 
-// TODO: Create a function to initialize app
+// TODO: Create a function to initialize app <- this comment came with the starter code
+// I feel like it's bad organization to do so many things in this one function, but whatever
 function init() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: questions[0]
-        },
-        {
-            type: "input",
-            name: "description",
-            message: questions[1]
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: questions[2]
-        },
-        {
-            type: "input",
-            name: "contribution",
-            message: questions[3]
-        },
-        {
-            type: "input",
-            name: "test",
-            message: questions[4]
-        }
-    ])
+    console.clear(); console.log(introString);
+    inquirer.prompt(contentQuestions)
     
 .then((answers) => {
+    exec('"./utils/generateMarkdown.js"', (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);})
     textToWrite =`
 # ${answers.title}
 
@@ -66,38 +94,10 @@ ${answers.contribution}
     
 ${answers.test}
 
-## License Information<hr>`;
+${generateMarkdown}`;
     writeToFile("README.md", textToWrite);
 })
 }
 
-// Function call to initialize app
+// Function call to initialize app <- this comment came with the starter code
 init();
-
-
-/*
-Code snippets that might come in handy later:
-
-The questions array as an object:
-
-const questions = {
-    title: "What is the TITLE of your project? ",
-    description: "What is the DESCRIPTION of your project? ",
-    usage: "What are the USAGE instructions for your project? ",
-    contribution: "How can users make a CONTRIBUTION? ",
-    test: "What are the TEST INSTRUCTIONS? "
-};
-
-The init function as a for...of loop:
-
-function init() {
-    for (const question of questions){
-        inquirer.prompt([
-        {
-            type: "input",
-            name: someUsersInputtedValue
-            message: question
-        }
-    ])}}
-
-*/
